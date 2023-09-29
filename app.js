2d07bc73e7462248c28273d8884be79b9955adbe
rich <dankwarrior6900@gmail.com>
2023-09-29 11:35:59 -0700

    Tried to make a password validator but working only after the form is submitted, rather than cancelling the submission to get user to reenter password again

--- app.js
+++ app.js
@@ -2,15 +2,34 @@
 
 document.addEventListener('DOMContentLoaded', function() {
 
-    let password = document.querySelector('#password').value;
-    let confirmPassword = document.querySelector('#confirm-password').value;
+    // let password = document.querySelector('#password').value;
+    // let confirmPassword = document.querySelector('#confirm-password').value;
     let message = document.querySelector('#error');
+    let form = document.querySelector('form');
 
 
-    if (confirmPassword === "" && password === "") {
-        message.innerText = '* Passwords do not match';
+    // if (confirmPassword === "" && password === "") {
+    //     message.innerText = '* Passwords do not match';
 
-    } else if (confirmPassword === password) {
-        message.innerText = ''; 
+    // } else if (confirmPassword === password) {
+    //     message.innerText = ''; 
+    // };
+
+    form.addEventListener('submit', matchPassword);
+
+    function matchPassword() {
+        let password1 = document.querySelector('#password').value;
+        let password2 = document.querySelector('#confirm-password').value;
+
+        if (password1 === "") {
+            message.innerText = '* Enter a password';
+        } else if (password2 === '') {
+            message.innerText = '* Enter confirm password';
+        } else if (password1 !== password2) {
+            message.innerText = '* Passwords do not match';
+            return false;
+        } else {
+            return true;
+        };
     };
 });
