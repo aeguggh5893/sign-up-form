9d9a271c6b618fca8d1d847d828607a63cd787c0
rich <dankwarrior6900@gmail.com>
2023-09-28 21:06:31 -0700

    Restructure second section and added button and text to the third section

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
 "use strict"
 
-console.log('hi');
+document.addEventListener('DOMContentLoaded', function() {
+
+    let password = document.querySelector('#password').value;
+    let confirmPassword = document.querySelector('#confirm-password').value;
+    let message = document.querySelector('#error');
+
+
+    if (confirmPassword === "" && password === "") {
+        message.innerText = '* Passwords do not match';
+
+    } else if (confirmPassword === password) {
+        message.innerText = ''; 
+    };
+});
